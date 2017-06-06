a1d7cdd9dfc7ea3c96bd54ca83004d1498b9b0ac
vedhanayaki <vedhanayaki1996@gmail.com>
2017-06-06 14:29:06 +0530

    Add files via upload

---
+++ natural.java
@@ -0,0 +1,16 @@
+package natural;
+
+public class natural {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("values");
+		int temp=0;
+		for(int i=0;i<30;i++){
+	temp=temp+i;		
+		}
+		System.out.println("value");
+
+	}
+
+}
